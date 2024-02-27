bd0164277e4ea5d36ddfa0edba071abed04d63b2
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-27 10:10:33 -0300

    Primeiro commit

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+// const mongoose = require('mongoose');
+
+// // Função para conexão ao Banco de Dados
+// const connectDB = async () => {
+//     try {
+//         mongoose.set('strictQuery', false);
+//         const conn = await mongoose.connect(process.env.URI);
+//         console.log(`Banco de dados conectado: ${conn.connection.host}`);
+//     } 
+
+//     catch (error) {
+//         console.log(error);
+//     }
+// };
+
+// module.exports = connectDB;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 5000 || process.env.PORT;
+const db = require('./database/db.js');
+// // Exportando função para conexão ao Banco de Dados
+// db();
+
+
+app.listen(PORT, () => {
+    console.log(`Rodando na Port ${PORT}`);
+});
